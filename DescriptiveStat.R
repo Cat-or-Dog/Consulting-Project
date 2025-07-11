b0d120ebe9c1f38030d09ed047aa442066bfa2a4
XU <66640182+Cat-or-Dog@users.noreply.github.com>
2025-07-12 01:40:33 +0800

    Update DescriptiveStat.R

--- DescriptiveStat.R
+++ DescriptiveStat.R
@@ -2,14 +2,11 @@ library(tidyverse)
 library(lubridate)
 
 # Specify the file path
-file_1 <- "G:/Consulting/data/data_1.rds"
-file_2 <- "G:/Consulting/data/data_2.rds"
+file_1 <- "./data_1.rds"
+file_2 <- "./data_2.rds"
 
-scalar_covariate_1 <- "G:/Consulting/data/scalar_covariates_1.rds"
-scalar_covariate_2 <- "G:/Consulting/data/scalar_covariates_2.rds"
-
-# # save as RDS file
-# saveRDS(data_1[1, ], file = "C:/Users/Xu Yuan/Documents/file_3")
+scalar_covariate_1 <- "./scalar_covariates_1.rds"
+scalar_covariate_2 <- "./scalar_covariates_2.rds"
 
 # Read the .RDS file
 data_1 <- readRDS(file_1)
@@ -17,19 +14,6 @@ data_2 <- readRDS(file_2)
 
 scalar_covariate_1 <- readRDS(scalar_covariate_1)
 scalar_covariate_2 <- readRDS(scalar_covariate_2)
-
-# Now you can use the data object in your R session
-# Function to extract time-series data from a list
-extract_timeseries <- function(data_list) {
-  time_vector <- data_list[1]      # First vector (time)
-  measurement_vector <- data_list[2]  # Second vector (measurements)
-  
-  # Create a data frame
-  data.frame(
-    time = time_vector,
-    measurement = measurement_vector
-  )
-}
 
 extract_min <- function(data) {
   res <- c(time = data[[1]]$arg[which.min(data[[1]]$value)], 
